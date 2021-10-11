b5bf54de6256cbfd295dad8e9cb095d6d5d285d0
jesshamby <jesshamby99@gmail.com>
2021-10-11 14:07:41 +0100

    Copied the index.php file to this branch so we could add to it

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <title>Capybara Canvas</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="normalize.css" />
+    <link rel="stylesheet" href="style.css" />
+</head>
+<body>
+<div id="toolbar">
+    <button id="painter"></button>
+    <button id="eraser"></button>
+</div>
+<main>
+    <h1>Capybara Canvas</h1>
+    <div id="canvas"></div>
+</main>
+</body>
+</html>
