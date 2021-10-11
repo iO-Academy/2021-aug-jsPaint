5e1f2fea53c4f02d14388d64dd186887575aaa3e
jesshamby <jesshamby99@gmail.com>
2021-10-11 15:35:01 +0100

    fixed canvas sizing so that it doesn't overflow onto the toolbar. added a a height value to the buttons.

--- index.php
+++ index.php
@@ -11,9 +11,11 @@
     <link rel="stylesheet" href="style.css" />
 </head>
 <body>
+<div>
     <div class="toolbar">
         <button class="painter"></button>
         <button class="eraser"></button>
+    </div>
     </div>
     <main>
         <h1>Capybara Canvas</h1>
