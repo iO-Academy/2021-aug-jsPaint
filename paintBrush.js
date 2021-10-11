9889bbdf1fbe933168f5437b20f81a27e6390ad2
Jake Rooms <jakeprooms@outlook.com>
2021-10-11 14:04:09 +0100

    added drawing evemts, accounted for not painting, set canvas height on doc

--- paintBrush.js
+++ paintBrush.js
@@ -1,10 +1,39 @@
 const canvas = document.querySelector('canvas')
 const ctx = canvas.getContext('2d')
 
-function onMouseMove(event){
+canvas.width = 500;
+canvas.height = 500;
+
+function drawLine(event){
     ctx.lineTo(event.offsetX, event.offsetY)
-    ctx.stroke()
+    if (!painting) {
+        // When not painting, begin a new path
+        ctx.beginPath();
+        ctx.moveTo(event.offsetX, event.offsetY);
+    } else {
+        ctx.lineTo(event.offsetX, event.offsetY)
+        ctx.stroke()
+    }
 }
 
-canvas.addEventListener('mousemove', onMouseMove)
+if (canvas) {
+    canvas.addEventListener('mousemove', drawLine);
+    // Start painting on click event
+    canvas.addEventListener('mousedown', startPainting);
+    // Stop painting on stopping clicking/off page
+    canvas.addEventListener('mouseup', stopPainting);
+    canvas.addEventListener('mouseleave', stopPainting);
+}
 
+let painting = false;
+
+function stopPainting() {
+    painting = false;
+}
+
+function startPainting() {
+    painting = true;
+    if (painting === true) {
+        canvas.classList.add('brush');
+    }
+}
