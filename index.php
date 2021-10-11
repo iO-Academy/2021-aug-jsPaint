0d14d1d7a07a782c93de2cc57bdbab0f0d840ea4
jesshamby <jesshamby99@gmail.com>
2021-10-11 13:43:14 +0100

    Added images to the buttons, styled the header q

--- index.php
+++ index.php
@@ -7,8 +7,8 @@
     <meta charset="UTF-8" />
     <title>Capybara Canvas</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-<!--    <link rel="stylesheet" href="normalize.css" />-->
-<!--    <link rel="stylesheet" href="style.css" />-->
+    <link rel="stylesheet" href="normalize.css" />
+    <link rel="stylesheet" href="style.css" />
 </head>
 <body>
     <div id="toolbar">
@@ -19,6 +19,5 @@
         <h1>Capybara Canvas</h1>
         <div id="canvas"></div>
     </main>
-
 </body>
 </html>
