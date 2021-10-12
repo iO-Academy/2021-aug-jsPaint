12d029ff52f058dd134b1cc1a0ddd3fe24351e6c
Jake Rooms <jakeprooms@outlook.com>
2021-10-12 16:52:01 +0100

    added disable canvas size if too small window, added tooltip to explain

--- index.php
+++ index.php
@@ -11,12 +11,16 @@
 <body>
 <div>
     <div class="toolbar">
+        <label for="sizeForm">Select size:
+            <p class="tooltiptext">Please enlarge your page for the larger options!</p>
+        </label>
         <select class="sizeForm" id="sizeForm">
             <option data-width="400" data-height="300">Small</option>
             <option data-width="800" data-height="500">Medium</option>
             <option data-width="1200" data-height="700">Large</option>
         </select>
-       <button class="painter"></button>
+
+        <button class="painter"></button>
        <button class="eraser"></button>
     </div>
     </div>
