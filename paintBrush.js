3775de158d3436a6f9f4fa4a55930fc82f5755ba
jesshamby <jesshamby99@gmail.com>
2021-10-12 16:27:31 +0100

    Got the print image code working from the previous commit so the drawing on the canvas is shown in the print window. Need to work to close the opened window.

--- paintBrush.js
+++ paintBrush.js
@@ -87,19 +87,18 @@ document.querySelector('.text').addEventListener('click', e => {
 // story 9
 document.querySelector('.print').addEventListener('click', e => {
     e.preventDefault()
-    // window.document.querySelector('.canvas').print()
+    e.stopPropagation()
 
     // Store DIV contents in the variable.
-    var canvasToPrint = document.getElementsByClassName('canvas')
-    console.log(canvasToPrint)
+    let canvasToPrint = document.querySelector('.canvas')
     // Create a window object.
-    var win = window.open('', '', 'height=700,width=700')
+    let win = window.open('', '', 'height=700,width=700')
     // Open the window. Its a popup window.
     // win.document.write("Hello World");
     win.document.write("<br><img src = '"+canvasToPrint.toDataURL()+"'/>")
     // Write contents in the new window.
+    win.setTimeout(() => win.print(), 0)
     win.document.close()
-    win.print()       // Finally, print the contents.
 })
 
 
