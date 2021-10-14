7fcbc8d30d10c3741f0d3da48557bfd027014e74
jesshamby <jesshamby99@gmail.com>
2021-10-14 15:51:55 +0100

    Merged main into the print story and sorted the printing of canvi.

--- paintBrush.js
+++ paintBrush.js
@@ -12,7 +12,8 @@ const textInput = document.querySelector('#textInput')
 const textSubmit = document.querySelector('#textSubmit')
 //Sets the connection to canvi
 const canvi = document.querySelector('.canvi')
-
+//Sets the connection to the print button
+const print = document.querySelector('.print')
 
 //Create a texts array to hold text objects
 let texts = []
@@ -264,25 +265,19 @@ textButton.addEventListener('click', e => {
     textInput.setAttribute('type', 'text')
     textSubmit.setAttribute('type', 'submit')
 })
-// story 9
-document.querySelector('.print').addEventListener('click', e => {
-    e.preventDefault()
 
-    // Store canvas contents in the variable canvasToPrint
-    let canvasToPrint = document.querySelector('.canvas')
+print.addEventListener('click', e => {
+    e.preventDefault()
     // Create a window object
     let win = window.open('', '', 'height=700,width=700')
     //display the contents of the canvas as an image in the window
-    win.document.write("<br><img src = '"+canvasToPrint.toDataURL()+"'/>")
+    win.document.write("<div style='position:relative'></div><br><img src = '"+canvas.toDataURL()+"'/><img src = '"+canvasTextC.toDataURL()+"' style='position: absolute; left: 0;'/></div>")
     // Print the contents of the window then close it
     win.setTimeout(() => {
         win.print()
         win.close()
     }, 0)
 })
-
-
-
 
 /** Function to disconnect lines when not painting, paint where mouse is when clicking down
  * Dependant on mode set, use black for brush and white for eraser
